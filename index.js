8c254802f6d0ba3fffd1eac7308b9a6544d3c3c5
hyperboink <hyperboink@gmail.com>
2023-06-13 17:44:55 +0800

    Omingle - Talk to Strangers!

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const hasRoomExceed = (room, limit) => io.sockets.adapter.rooms.get(room).size >= limit;
+const hasRoomExceedByPair = room => hasRoomExceed(room, 2);
+let index = 1;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        io.to(socket.currentRoom).emit('chat:disconnected');
+    });
+
+    socket.on('chat:matching', () => {
+        let room = "room:" + index;
+
+        socket.join(room);
+        socket.currentRoom = room;
+
+        if(hasRoomExceedByPair(socket.currentRoom)){
+            io.in(socket.currentRoom).emit('room:created', {
+                message: `<div class="intro">You can start chatting now.</div>`
+            });
+
+            index++;
+        }
+    });
+
+    socket.on('send-message', (data) => {
+        if(hasRoomExceedByPair(socket.currentRoom)){
+            socket.to(socket.currentRoom).emit('receive-message', data);
+        }
+    });
+});
+
+server.listen(8003, () => {
+  console.log('listening on *:8003');
+});
